cf7cb90d8cca80abe509a2eef48f7ec3dd4b416c
Amy Mitchneck <amitchneck70@stuy.edu>
2025-10-06 19:24:45 -0400

    added previous wirrten helper methods from ArrayMethods.java cw

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -8,7 +8,50 @@ public class ArrayDemo{
     //Compare Arrays.deeoToString(your2DArray) to YOUR arrayToString() method to make sure yours is correct
     //do not use any other Arrays.method()
    
-
+public static String arrToString(int[]ary){
+  String ans "["";
+    for (int i = 0; i < nums.length; i++) {
+      ans += ary[i];
+      if ( i <n nums.length - 1 ) {
+        ans += ", ";
+      }
+    }
+    ans += "]";
+  return ans;
+}
+//3. Write arrToString, with a 2D array parameter.
+//Note: Different parameters are allowed with the same function name.
+public static String arrToString2D(int[][]ary){
+  String result = "[";
+for(int i = 0; i <ary.length;i++){
+result += arrToString(ary[i]);
+if(i<ary.length-1)result += ",";
+}
+result += "]";
+return result;
+}
+/*Return the sum of all of the values in the 2D array */
+public static int arr2DSum(int[][]nums){
+  int sum = 0;
+for(int j= 0; j< nums.length;j++){
+for(int h = 0; h< nums[j].lengtj;h++){
+sum+= nums[j][h];
+}
+}
+  return sum;
+}
+/**Rotate an array by returning a new array with the rows and columns swapped.
+  * You may assume the array is rectangular and neither rows nor cols is 0.
+  * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+  */
+public static int[][] swapRC(int[][]nums){
+  int row = nums.length;
+int col = nums[0].length;
+int[][] result = new int[c][r];
+for(int r =0; r <row;r++){
+for(int c = 0; c<col; c++){
+result[c][r] = nums[r][c];
+}
  public static String htmlTable(int[][] nums){
 String html = "<table>"; // opening tab for the table
 for (int row = 0; row < nums.length; row++){ // loop through rows
